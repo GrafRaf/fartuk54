aaf35aef0592d6caca30bf35d3747afdbad6a33f
Rafael <grafraf@mail.ru>
2016-05-25 15:45:29 +0600

    13

--- scripts/projects.js
+++ scripts/projects.js
@@ -497,26 +497,35 @@ function applyBoxClick() {
             var data = $.data(t).model.model.data;
         }
         if (data){
-       		console.log(t);
             	$(t).magnificPopup({
 			items: {
       				src: t.children[0].src.replace('/min/','/'),
       				verticalFit: true,
-				markup: 
-					'<div class="mfp-figure">'+
-            				'<div class="mfp-close"></div>'+
-            				'<div class="mfp-img"></div>'+
-            				'<div class="mfp-bottom-bar">'+
-              				'<div class="mfp-title">'+
-              				t.children[0].src.substring(t.children[0].src.indexOf('/min/')).replace('/min/','').replace('.jpg','')+
-              				'</div>'+
-              				'<div class="mfp-counter"></div>'+
-            				'</div>'+
-          				'</div>'
+      				imgTitle: t.children[0].src.substring(t.children[0].src.indexOf('/min/')).replace('/min/','').replace('.jpg',''),
     			},            		
     			type: 'image',
 	    	   	closeOnContentClick: true,
 	        	mainClass: 'mfp-img-mobile',
+			image: {
+  				markup: '<div class="mfp-figure">'+
+            				'<div class="mfp-close"></div>'+
+            				'<div class="mfp-img"></div>'+
+            				'<div class="mfp-bottom-bar">'+
+              				'<div class="mfp-title"></div>'+
+              				'<div class="mfp-counter"></div>'+
+            				'</div>'+
+          				'</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+		  		cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+	
+  				//titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
+  				// Or the function that should return the title. For example:
+  				titleSrc: function(item) {
+  				   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+  				},
+  				verticalFit: true, // Fits image in area vertically
+  				tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+			}	        	
         	});                        
         } //alert(JSON.stringify(data));
     })
